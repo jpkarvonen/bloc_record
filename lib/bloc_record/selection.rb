e8a81846569dd30265273bfe762b7d3c8c46f524
John <jpkarvonen@gmail.com>
2018-10-01 11:19:49 -0400

    checkpoint complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -20,7 +20,7 @@ module Selection
 
   def find_one(id)
     return "Error: invalid id" if invalid_id?(id)
-    row = connection.get_row <<-SQL
+    row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE id = #{id}
     SQL
@@ -65,12 +65,13 @@ module Selection
   end
 
   def take(num=1)
-    unless num.is_a? Integer return "Error: Please enter a number"
+    return "Error: Please enter a number" unless num.class == Integer
     if num > 1
       rows = connection.execute <<-SQL
         SELECT #{columns.join ","} FROM #{table}
         ORDER BY random()
         LIMIT #{num}
+    end
       SQL
 
       rows_to_array(rows)
@@ -119,11 +120,72 @@ module Selection
     find_by(extract_attribute(method), args)
   end
 
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
+    end
+
+    sql = <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+  def order(*args)
+    if args.count > 1
+      order = args.join(",")
+    else
+      order = args.first.to_s
+    end
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{order};
+    SQL
+    rows_to_array(rows)
+  end
+
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+      SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+        SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+        SQL
+      end
+    end
+
+    rows_to_array(rows)
+  end
 
   private
 
   def invalid_id?(id)
-    return false if id.is_a? Integer && id >= 0
+    if (id.is_a? Integer) && id >= 0
+      return false
+    end
     true
   end
 
